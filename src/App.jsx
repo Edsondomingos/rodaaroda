6d5582fea7dc70b9413773ac75a1b410118925e3
Edson Domingos <edomingoswork@gmail.com>
2025-04-21 15:45:54 -0300

    correção nova palavra e adiciona opcao de premio

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,8 @@ function App() {
   const [verificaLetra, setVerificaLetra] = useState('')
   const [addJogo, setAddJogo] = useState(true)
   const [indicePalavra, setIndicePalavra] = useState(null)
+  const [numero, setNumero] = useState(-5)
+  const [modal, setModal] = useState(false)
 
   const verificar = (e) => {
     let letraEncontrada = letrasEncontradas
@@ -57,6 +59,7 @@ function App() {
         setIndicePalavra(indice)
         setPalavraAtual(palavras[indice][0])
         setDicaAtual(palavras[indice][1])
+        setLetrasEncontradas([])
       } else {
         setAddJogo(true)
       }
@@ -85,14 +88,34 @@ function App() {
       setPalavraAtual('')
       setDicaAtual('')
       setAddJogo(true)
+      window.location.reload()
     }
   }
 
+
+  const escolher = () => {
+    setNumero(-5)
+    console.log(document.querySelector('.seta'))
+
+    setTimeout(() => {
+      setNumero(85)
+      console.log(document.querySelector('.seta'))
+    }, 2000)
+
+    setTimeout(() => {
+      const n = Math.floor((Math.random() * 90) - 5)
+      setNumero(n)
+    }, 3000)
+
+    console.log(document.querySelector('.seta'))
+  }
+
+
   return (
-    <>
+    <section className='container'>
 
       <section className="ajuda centralizar">
-        <button title='Ajuda: Entenda o jogo'>?</button>
+        <button title='Ajuda: Entenda o jogo' onClick={() => setModal(true)}>?</button>
         <button title='Nova palavra' onClick={atualizarpalavra}>@</button>
         <button title='Reutilizar palavras' onClick={reutilizar}>#</button>
         <button title='Começar tudo do zero' onClick={restart}>0</button>
@@ -110,6 +133,8 @@ function App() {
                     letrasEncontradas.indexOf(index) === -1 ?
                       <p className='palavra-espaco' key={index}>{letra}</p>
                       :
+                      // letra === '-' ?
+                      // <p style={{color: '#fff'}} key={index}>ok</p> :
                       <p className='palavra-letra' key={index}>{letra}</p>
                   )
 
@@ -172,16 +197,68 @@ function App() {
               }}>Sem mais palavras ou dicas</button>
             </div>
             :
-            // <div className="roleta-circulo">
-            //   <p className="roleta-opcao premio1 centralizar">100</p>
-            //   <p className="roleta-opcao premio2 centralizar">100</p>
-            //   <p className="roleta-opcao premio3 centralizar">100</p>
-            // </div>
-            <></>
+            <div className="roleta-circulo">
+              <div className="roleta-opcao centralizar">
+                <p className='roleta-opcao-nome'>3 Confeitos</p>
+              </div>
+              <div className="roleta-opcao centralizar">
+                <p className='roleta-opcao-nome'>2 Confeitos</p>
+              </div>
+              <div className="roleta-opcao centralizar">
+                <p className='roleta-opcao-nome'>1 Confeitos</p>
+              </div>
+              <div className="roleta-opcao centralizar">
+                <p className='roleta-opcao-nome'>Perdeu Tudo</p>
+              </div>
+              <div className="roleta-opcao centralizar">
+                <p className='roleta-opcao-nome'>1 Doce</p>
+              </div>
+              <div className="roleta-opcao centralizar">
+                <p className='roleta-opcao-nome'>2 Doce</p>
+              </div>
+              <div className="roleta-opcao centralizar">
+                <p className='roleta-opcao-nome'>3 Doce</p>
+              </div>
+              <div className="roleta-opcao centralizar">
+                <p className='roleta-opcao-nome'>Passa a Vez</p>
+              </div>
+              <div className="roleta-opcao centralizar">
+                <p className='roleta-opcao-nome'>1 Pirulito</p>
+              </div>
+              <div className="roleta-opcao centralizar">
+                <p className='roleta-opcao-nome'>2 Pirulitos</p>
+              </div>
+              <div className='seta' style={{ left: numero + '%', transition: '5s' }} onClick={() => escolher()}></div>
+            </div>
         }
+
       </section>
 
-    </>
+
+      {
+        modal ?
+          <section className="modal">
+            <article className='modal-conteudo '>
+
+              <div>
+                <h1>Regras</h1>
+                <ul>
+                  <li>Após os participantes serem escolhidos, sera estabelecido a sequencia de jogadores (1º, 2º ...)</li>
+                  <li>Antes de poder falar a letra, o participante (grupo), precisar saber o premio por acertar a letra</li>
+                  <li>Cada participante pode falar apenas uma letra por vez após o
+                    premio acumulativo ser mostrado aleatoriamente</li>
+                  <li>O participante pode arriscar a palavra apenas se sobrarem no minimo 3 letras</li>
+                  <li>Apenas um unico participante por rodada ganha o seu proprio premio acumulado na rodada</li>
+                  <li>Ao fim, o vencedor será o participante que tiver a maior quantidade de premios acumulados em todas as rodadas.</li>
+                  <li>Entre as opções de prêmios, há opções de PERDER ou PASSAR A VEZ, sem ter chance falar nenhuma letra ou arriscar a palavra nas respectivas opções.</li>
+                </ul>
+              </div>
+            </article>
+            <button onClick={() => setModal(false)} className='btn-fechar'>Fechar</button>
+          </section>
+          :
+          ""}
+    </section>
   )
 }
 
